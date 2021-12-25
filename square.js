eb4b140599da20c61c89d0a01d47e410096753b8
Tarun123456789-trn <67340592+Tarun123456789-trn@users.noreply.github.com>
2021-12-25 14:47:14 +0530

    Add files via upload
    Square of number javascript

---
+++ square.js
@@ -0,0 +1,21 @@
+//function square(n){
+//	return n * n;
+//}
+
+//console.log(square(5));
+
+
+//const square = function(n){
+//	return n * n;
+//}
+//var x = square(4);
+
+//console.log(x);
+
+//square(5);
+
+console.log(square(5));
+function square(n){
+	return n*n
+}
+
